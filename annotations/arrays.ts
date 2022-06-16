f35c5c0194e7185fb7614a698b1b8050b10be86e
Ridercogswell <ridercogswell@gmail.com>
2022-06-15 22:29:29 -0700

    arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,6 @@
+// type inference works here
+const carMakers = ['ford', 'chevy', 'toyota'];
+const dates = [new Date(), new Date()];
+
+// when we want an array of arrays holding strings 
+const carsByMake: string[][] = []
