8cfdcc4a7fde20e760ba440e1b672ddb6a3c5589
Ridercogswell <ridercogswell@gmail.com>
2022-06-15 21:44:11 -0700

    type inference

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -36,5 +36,33 @@ let point: { x: number; y: number } = {
               // type annotations to left, funct  to right of =
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
-  
+}
+
+// when to use TA's
+
+// 1) function that returns any // AVOID AT ALL COSTS
+// whenever you use JSON.parse, it returns an 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number, y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) when we declare a variable on one line and initialize later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cant be inferred // like an or statement
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;  
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    // because type inference has a hard time inferring values in times like this, when it can be affected, we add TA's to help it 
+    numberAboveZero = numbers[i];
+  }
 }
