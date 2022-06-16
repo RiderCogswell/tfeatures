d7baa70c40e9660a8fdf6239eb47309e22a8a4b5
Ridercogswell <ridercogswell@gmail.com>
2022-06-16 11:21:40 -0700

    tuples

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -4,3 +4,19 @@ const dates = [new Date(), new Date()];
 
 // when we want an array of arrays holding strings 
 const carsByMake: string[][] = []
+
+const cars = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incompatible values
+carMakers.push(adevgjhvd);
+
+// help with map/ foreach/ reduce functions
+carMakers.map((cars: string): string => {
+  return cars.toUpperCase();
+});
+
+// flexible types // when arrays tsake multiple types values
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-23');
+importantDates.push(new Date())
