5a548c0bca2b1e12675107af1d43045f5007ac18
Ridercogswell <ridercogswell@gmail.com>
2022-06-17 16:21:06 -0700

    interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,11 +1,18 @@
+// reusable name
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+// must have same property names as interface
 const oldCivic = {
   name: 'civic',
   year: 2004,
   broken: true
 };
 
-
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean  }): void => {
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Name: ${vehicle.year}`);
   console.log(`Name: ${vehicle.broken}`);
