fe9cb194e636c85e08df4304164cd0fae85f57ec
Ridercogswell <ridercogswell@gmail.com>
2022-06-18 11:20:10 -0700

    class protection

--- classes.ts
+++ classes.ts
@@ -1,24 +1,25 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 };
 
 // extends allowsthe methods to be passed into the new class through basic inheritance
 class Cars extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('vroom');
+  }
+
+  // can be called even though it is private
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const vehicle = new Vehicle();
-vehicle.drive()
-vehicle.honk()
+// wont work because not explicit children
+// vehicle.honk()
 
 const carss = new Cars();
-
-carss.drive();
-carss.honk()
+// carss.honk()
