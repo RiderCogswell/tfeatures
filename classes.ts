23628b247ccfa76f073af28a081d6a87f7cc209c
Ridercogswell <ridercogswell@gmail.com>
2022-06-18 11:10:50 -0700

    class extension

--- classes.ts
+++ classes.ts
@@ -7,6 +7,18 @@ class Vehicle {
   }
 };
 
+// extends allowsthe methods to be passed into the new class through basic inheritance
+class Cars extends Vehicle {
+  drive(): void {
+    console.log('vroom');
+  }
+}
+
 const vehicle = new Vehicle();
 vehicle.drive()
 vehicle.honk()
+
+const carss = new Cars();
+
+carss.drive();
+carss.honk()
